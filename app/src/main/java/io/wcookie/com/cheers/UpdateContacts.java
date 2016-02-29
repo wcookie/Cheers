0300603bf3d4880e174f03e67dae327ae4b7300d
Wyatt Cook <wyattcook2018@u.northwestern.edu>
2016-02-28 20:59:50 -0600

    First draft of contacts, but with email instead of phone #

--- app/src/main/java/io/wcookie/com/cheers/UpdateContacts.java
+++ app/src/main/java/io/wcookie/com/cheers/UpdateContacts.java
@@ -1,9 +1,95 @@
 package io.wcookie.com.cheers;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 /**
  * Created by wcook on 2/28/2016.
  */
-public class UpdateContacts {
+public class UpdateContacts extends AppCompatActivity {
 
+    private Button primaryContact;
+    private Button secondaryContact;
+    private Button submitButton;
+    private static final String TAG = "UpdateContacts";
+    private static final int CONTACT_PICKER_RESULT = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_contacts);
+        primaryContact = (Button) findViewById(R.id.primaryContact);
+
+        //Creates contact selecter when contact "+" is clicked
+        primaryContact.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                        ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+            }
+        });
+
+
+//Called when contact selecter is finished and returns the email address of the contact
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    Cursor cursor = null;
+                    String email = "";
+                    try {
+                        Uri result = data.getData();
+                        Log.v(TAG, "Got a contact result: "
+                                + result.toString());
+
+                        // get the contact id from the Uri
+                        String id = result.getLastPathSegment();
+
+                        // query for everything email
+                        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                                null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[]{id},
+                                null);
+
+                        int emailIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
+
+                        // let's just get the first email
+                        if (cursor.moveToFirst()) {
+                            email = cursor.getString(emailIdx);
+                            Log.v(TAG, "Got email: " + email);
+                        } else {
+                            Log.w(TAG, "No results");
+                        }
+                    } catch (Exception e) {
+                        Log.e(TAG, "Failed to get email data", e);
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                        //EditText emailEntry = (EditText) findViewById(R.id.contactText);
+                        //emailEntry.setText(email);
+                        if (email.length() == 0) {
+                            Toast.makeText(this, "No email found for contact.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    break;
+            }
+        }
+    }
+}
 
 }
+
